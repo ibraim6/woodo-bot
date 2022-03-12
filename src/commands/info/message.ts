5b87614c591943e3d32f19a2755fd7cb0a6406a1
ibraim6 <ibrahim.shaz670@gmail.com>
2022-03-12 18:21:40 +0530

    added embed

--- src/commands/info/message.ts
+++ src/commands/info/message.ts
@@ -1,32 +1,98 @@
-import { TextChannel } from 'discord.js';
+import type { TextChannel, ColorResolvable } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
 import { Command } from './../../structures/Command';
 export default new Command({
-  name: 'message',
-  description: 'Message In Any Channel',
+  name: 'send',
+  description: 'Send Messages',
   userPermissions: ['MANAGE_MESSAGES'],
   options: [
     {
-      name: 'channel',
-      description: 'The Channel Which You Want to send a message',
-      required: true,
-      type: 'CHANNEL',
+      name: 'embed',
+      description: 'send embed',
+      type: 'SUB_COMMAND',
+      options: [
+        {
+          name: 'channel',
+          description: 'enter a channel',
+          type: 'CHANNEL',
+          required: true,
+        },
+        {
+          name: 'title',
+          description: 'The title for the embed',
+          type: 'STRING',
+          required: true,
+        },
+        {
+          name: 'description',
+          description: 'Description for your embed',
+          type: 'STRING',
+          required: true,
+        },
+      ],
     },
     {
-        name: 'message',
-        description: 'Type Message To Send',
-        required: true,
-        type: 'STRING'
-    }
+      name: 'message',
+      description: 'Just a message',
+      type: 'SUB_COMMAND',
+      options: [
+        {
+          name: 'channel',
+          description: 'The Channel Which You Want to send a message',
+          type: 'CHANNEL',
+          required: true,
+        },
+        {
+          name: 'message',
+          description: 'Type Message To Send',
+          type: 'STRING',
+          required: true,
+        },
+      ],
+    },
   ],
   run: async ({ interaction }) => {
     const channel = interaction.options.getChannel('channel');
-    const message = interaction.options.getString('message')
-    const channelID = interaction.client.channels.cache.get(channel.id) as TextChannel
-    if (!channelID.send) return interaction.followUp({content: 'Not A Valid **Channel**'})
-    channelID.send({content: `${message}`}).then(()=> {
-      interaction.followUp({content: "Message Send **Successfully**"})
-    }).catch(()=> {
-      interaction.followUp({content: 'Not Enough **Permissions**'})
-    })
+    const subcmd = interaction.options.getSubcommand();
+    if (subcmd === 'message') {
+      const message = interaction.options.getString('message');
+      const channelID = interaction.client.channels.cache.get(
+        channel.id
+      ) as TextChannel;
+      if (!channelID.send)
+        return interaction.followUp({ content: 'Not A Valid **Channel**' });
+      channelID
+        .send({ content: `${message}` })
+        .then(async () => {
+          await interaction.followUp({
+            content: 'Message Send **Successfully**',
+          });
+        })
+        .catch(() => {
+          interaction.followUp({ content: 'Not Enough **Permissions**' });
+        });
+    } else if (subcmd === 'embed') {
+      const title = interaction.options.getString('title');
+      const description = interaction.options.getString('description');
+      const embed = new MessageEmbed()
+        .setTitle(`${title}`)
+        .setColor('#00acff')
+        .setDescription(`${description}`);
+      const channelID = interaction.client.channels.cache.get(
+        channel.id
+      ) as TextChannel;
+      if (!channelID.send)
+        return interaction.followUp({ content: 'Not A Valid **Channel**' });
+      channelID
+        .send({ embeds: [embed] })
+        .then(async () => {
+          await interaction.followUp({
+            content: 'Embed Send **Successfully**',
+          });
+        })
+        .catch(() => {
+          interaction.followUp({ content: 'Not Enough **Permissions**' });
+        });
+    }
   },
 });
